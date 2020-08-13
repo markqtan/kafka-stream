7c7eb9ae06f4a41b3db408e2c7dd655486d233fb
markqtan <markqtan@hotmail.com>
2020-08-13 11:51:12 -0700

    refactor SimpleProducer extracting send method

--- src/main/java/com/markqt/kafka/producer/SimpleProducer.java
+++ src/main/java/com/markqt/kafka/producer/SimpleProducer.java
@@ -25,22 +25,25 @@ public class SimpleProducer {
 
     public static void main(String[] args) {
 
-        Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-//        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("key.serializer", "com.markqt.kafka.ser.PurchaseKeySerializer");
-        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("acks", "1");
-        properties.put("retries", "3");
-        properties.put("compression.type", "snappy");
-        //This line in for demonstration purposes
-        properties.put("partitioner.class", PurchaseKeyPartitioner.class.getName());
-        properties.put("key.serializer.class", "kafka.serializer.DefaultEncoder");
+    	Properties propsOverride = new Properties();
+    	propsOverride.put("key.serializer", "com.markqt.kafka.ser.PurchaseKeySerializer");
 
         PurchaseKey key = new PurchaseKey("12334568", new Date());
 
-        try(Producer<PurchaseKey, String> producer = new KafkaProducer<>(properties)) {
-            ProducerRecord<PurchaseKey, String> record = new ProducerRecord<>("testTopic", key, "value");
+        send("testTopic", propsOverride, key, "value-2");
+
+    }
+
+    public static<K, V> void send(String topic, K key, V value) {
+    	send(topic, null, key, value);
+    }
+	public static<K, V> void send(String topic, Properties propsOverride, K key, V value) {
+		Properties properties = config();
+		if(propsOverride != null) {
+			properties.putAll(propsOverride);
+		}
+		try(Producer<K, V> producer = new KafkaProducer<>(properties)) {
+            ProducerRecord<K, V> record = new ProducerRecord<>(topic, key, value);
 
             Callback callback = (metadata, exception) -> {
                 if (exception != null) {
@@ -50,8 +53,21 @@ public class SimpleProducer {
 
             Future<RecordMetadata> sendFuture = producer.send(record, callback);
         }
+	}
 
-    }
+	private static Properties config() {
+		Properties properties = new Properties();
+		properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("acks", "1");
+        properties.put("retries", "3");
+        properties.put("compression.type", "snappy");
+        //This line in for demonstration purposes
+        properties.put("partitioner.class", PurchaseKeyPartitioner.class.getName());
+        properties.put("key.serializer.class", "kafka.serializer.DefaultEncoder");
+		return properties;
+	}
 
 
 }
